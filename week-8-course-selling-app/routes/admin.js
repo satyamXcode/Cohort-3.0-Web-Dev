3ba8d15a985b11134ae485dac7d0b33a710d565c
satyamXcode <jaiswalsatyam876@gmail.com>
2025-04-15 17:31:20 +0530

    Completed backend for admin end

--- week-8-course-selling-app/routes/admin.js
+++ week-8-course-selling-app/routes/admin.js
@@ -70,15 +70,40 @@ adminRouter.post("/course", adminMiddleware, async function(req, res){
     })
 })
 
-adminRouter.put("/course", function(req, res){
+adminRouter.put("/course", adminMiddleware, async function(req, res){
+
+    const adminId = req.userId;
+
+    const { title, description, imageUrl, price, courseId } = req.body;
+
+    const course = await courseModel.updateOne({
+        _id: courseId,
+        creatorId: adminId
+    },
+        {
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+        price: price
+    })
+
     res.json({
-        message: "course endpoint"
+        message: "course upated",
+        courseId: courseModel._id
     })
 })
 
-adminRouter.get("/course/bulk", function(req, res){
+adminRouter.get("/course/bulk", adminMiddleware, async function(req, res){
+
+    const adminId = req.userId;
+
+    const courses = await courseModel.find({
+        creatorId: adminId
+    });
+
     res.json({
-        message: "bulk endpoint"
+         message: "Your courses",
+         courses
     })
 })
 
