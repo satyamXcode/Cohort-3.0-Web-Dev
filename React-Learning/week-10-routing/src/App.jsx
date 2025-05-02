fa8d656f2129f67db8379e143cf57d938f9e877f
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-03 04:37:10 +0530

    Learning: Week-10-Routing

---
+++ React-Learning/week-10-routing/src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment, useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Fragment>
+       <h1>Hello, world!</h1>
+    </Fragment>
+  )
+}
+
+export default App
