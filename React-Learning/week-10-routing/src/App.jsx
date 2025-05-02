818282917612712494fa04c45148571a8c33a761
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-03 04:58:25 +0530

    Routing implementation

--- React-Learning/week-10-routing/src/App.jsx
+++ React-Learning/week-10-routing/src/App.jsx
@@ -1,13 +1,45 @@
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Fragment>
-       <h1>Hello, world!</h1>
+
+       <BrowserRouter>
+
+         <Routes>
+
+            <Route path="/neet/online-coaching-class-11" element={<Class11Program />}/>
+            <Route path="/neet/online-coaching-class-12" element={<Class12Program />}/>
+            <Route path="/" element={<Landing />}/>
+
+         </Routes>
+
+       </BrowserRouter>
+
     </Fragment>
   )
 }
+
+function Landing() {
+   return <Fragment>
+    <h1>Welcome to allen</h1>
+   </Fragment>
+}
+
+function Class11Program() {
+  return <Fragment>
+   <h1>Neet programs for class 11th</h1>
+  </Fragment>
+}
+
+function Class12Program() {
+  return <Fragment>
+   <h1>Neet programs for class 12th</h1>
+  </Fragment>
+}
+
 
 export default App
