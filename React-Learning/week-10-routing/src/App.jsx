9e8e6ec3c5fd91a48cf6049795f4d45d6e45d339
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-03 06:19:33 +0530

    Implement Layout

--- React-Learning/week-10-routing/src/App.jsx
+++ React-Learning/week-10-routing/src/App.jsx
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 
 
 function App() {
@@ -9,17 +9,16 @@ function App() {
 
        <BrowserRouter>
 
-         <Link to="/"> Allen </Link>
-         |
-         <Link to="/neet/online-coaching-class-11"> Class 11 </Link>
-         |
-         <Link to="/neet/online-coaching-class-12"> Class 12 </Link>
-
          <Routes>
+
+         <Route path="/" element={<Layout />}>
 
             <Route path="/neet/online-coaching-class-11" element={<Class11Program />}/>
             <Route path="/neet/online-coaching-class-12" element={<Class12Program />}/>
             <Route path="/" element={<Landing />}/>
+            <Route path="*" element={<ErrorPage />}/>
+
+          </Route>
 
          </Routes>
 
@@ -28,6 +27,43 @@ function App() {
     </Fragment>
   )
 }
+
+function Layout(){
+  return <Fragment style={{height: "100vh", background: "yellow"}}>
+    <Header />
+    <div style={{height : "90vh", background: "red"}}>
+      <Outlet />
+    </div>
+    Footer || License-Owner
+  </Fragment>
+}
+
+function Header(){
+  return <Fragment>
+
+         <Link to="/"> Allen </Link>
+         |
+         <Link to="/neet/online-coaching-class-11"> Class 11 </Link>
+         |
+         <Link to="/neet/online-coaching-class-12"> Class 12 </Link>
+
+  </Fragment>
+}
+
+function ErrorPage() {
+
+  const navigate = useNavigate();
+
+  function redirectUser() {
+     navigate("/")
+  }
+
+  return <Fragment>
+   <h1>Sorry page not found</h1>
+   <button onClick={redirectUser}>Go back to landing page</button>
+  </Fragment>
+}
+
 
 function Landing() {
    return <Fragment>
