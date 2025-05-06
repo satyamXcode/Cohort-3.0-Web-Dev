293853cead20dfaf77f9364ec5e07bfcf0db723a
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-06 19:01:53 +0530

    Created seprate folder to store our custom hooks

--- React-Learning/week-11-react-1/src/App.jsx
+++ React-Learning/week-11-react-1/src/App.jsx
@@ -1,22 +1,13 @@
 import { Fragment, useState, useEffect } from 'react'
+import { usePostTitle } from './hooks/useFetch'
 
 function App() {
 
-  const [post, setPost] = useState({});
-
-  async function getPosts(){
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-    const json = await response.json();
-    setPost(json);
-  }
-
-  useEffect(() => {
-    getPosts();
-  }, [])
+  const postTitle = usePostTitle();
 
   return (
     <Fragment>
-       {post.title}
+       {postTitle}
     </Fragment>
   )
 }
