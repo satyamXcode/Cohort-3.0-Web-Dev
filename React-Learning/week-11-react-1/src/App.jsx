1aad049e89acc4bc2ac22b92f63138ce9b084e95
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-06 18:12:46 +0530

    backend Request: fetch()

--- React-Learning/week-11-react-1/src/App.jsx
+++ React-Learning/week-11-react-1/src/App.jsx
@@ -1,27 +1,22 @@
-import { Fragment, useState } from 'react'
-
-// custom hook
-function useCounter() {
-   const [count, setCount] = useState(0);
-
-   function increaseCount(){
-     setCount(count + 1);
-     // setCount(c => c + 1)
-   }
-   
-   return {
-     count: count,
-     increaseCount: increaseCount
-   }
-}
+import { Fragment, useState, useEffect } from 'react'
 
 function App() {
 
-  const {count, increaseCount} = useCounter();
+  const [post, getPosts] = useCounter({});
+
+  async function getPosts(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const json = await response.json();
+    setPost(json);
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [])
 
   return (
     <Fragment>
-       <button onClick={increaseCount}>Increase {count}</button>
+       {post.title}
     </Fragment>
   )
 }
