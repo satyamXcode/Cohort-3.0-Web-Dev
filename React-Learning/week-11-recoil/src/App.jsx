dd43fd8a5de258ff02c68fb04b231a4d0b6896f7
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-10 03:09:29 +0530

    Implement row state

--- React-Learning/week-11-recoil/src/App.jsx
+++ React-Learning/week-11-recoil/src/App.jsx
@@ -1,15 +1,63 @@
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
+
+   return (
+     <Fragment>
+       <Counter />
+     </Fragment>
+   )
+}
+
+function Counter() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-       <h1>hello, world!</h1>
-    </>
+    <Fragment>
+       <CurrentCount count={count} />
+       <br />
+       <Increase setCount={setCount} />
+       <Decrease setCount={setCount} />
+    </Fragment>
+  )
+}
+
+function CurrentCount({count}){
+  return (
+    <Fragment>
+       {count}
+    </Fragment>
+  )
+}
+
+function Decrease({setCount}){
+   
+   function decrease(){
+      setCount(c => c - 1);
+   }
+
+   return ( 
+   <Fragment>
+      <button onClick={decrease}>Decrease</button>
+   </Fragment>
+   )
+}
+
+function Increase({setCount}){
+
+  function increase(){
+    setCount(c => c + 1);
+ }
+
+
+  return (
+    <Fragment>
+      <button onClick={increase}>Increase</button>
+    </Fragment>
   )
 }
 
