f96377cfd224a4ee7a711be66cd179ef96e8608e
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-11 04:17:53 +0530

    Code added

--- React-Learning/week-11-recoil-offline/src/App.jsx
+++ React-Learning/week-11-recoil-offline/src/App.jsx
@@ -2,7 +2,7 @@ import { Fragment, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { RecoilRoot, useRecoilValue } from 'recoil'
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil'
 
 function App() {
   return<Fragment>
@@ -15,17 +15,22 @@ function App() {
 function MainApp(){
 
   const networkNotificationCount = useRecoilValue(networkAtom);
+  const jobsAtomCount = useRecoilValue(jobsAtom);
+  const notificatonsAtomCount = useRecoilValue(notificatonsAtom);
+  const [messagingAtomCount, setMessagingAtomCount] = useRecoilState(messagingAtom);
 
   return (
     <Fragment>
         <button>Home</button>
 
-        <button>My network ()</button>
-        <button>Jobs ()</button>
-        <button>Messaging ()</button>
-        <button>Notification ()</button>
+        <button>My network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+        <button>Jobs {jobsAtomCount}</button>
+        <button>Messaging {notificatonsAtomCount}</button>
+        <button>Notification {messagingAtomCount}</button>
 
-        <button>Me</button>
+        <button onClick={() => {
+          setMessagingAtomCount(messagingAtomCount+1);
+        }}>Me</button>
     </Fragment>
   )
 }
