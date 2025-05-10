3f22f7921ecbacec0336db5698c6b0bf15d3c846
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-10 18:24:11 +0530

    Implement : Recoil-> Atom & Selector

--- React-Learning/week-11-recoil/src/App.jsx
+++ React-Learning/week-11-recoil/src/App.jsx
@@ -1,57 +1,68 @@
-import { useState } from 'react'
+import { useEffect, useState, memo, Fragment } from 'react';
 import './App.css'
-import { RecoilRoot, atom, useRecoilValue, useSetRecoilState } from 'recoil';
-import { counterAtom } from './store/atoms/counter';
+import { RecoilRoot, useSetRecoilState, useRecoilValue } from 'recoil';
+import { counterAtom, evenSelector } from './store/atoms/counter';
 
-function App() {
-
+function App(){
+    
   return (
-    <RecoilRoot>
-     <Counter />
-    </RecoilRoot>
+    <Fragment>
+
+      <RecoilRoot>
+
+       <Buttons />
+       <Counter />
+       <IsEven />
+
+      </RecoilRoot>
+
+    </Fragment>
+  )
+}
+
+function Buttons() {
+
+  const setCount = useSetRecoilState(counterAtom);
+   
+  function increase() {
+     setCount(c => c + 2)
+  }
+
+  function decrease(){
+     setCount(c => c - 1)
+  }
+
+  return ( 
+  <Fragment>
+     <button onClick={increase}>Increase</button>
+     <button onClick={decrease}>Decrease</button>
+  </Fragment>
   )
 }
 
 function Counter() {
 
-  return <div>
-    <CurrentCount />
-    <Increase />
-    <Decrease />
-  </div>
+   const count = useRecoilValue(counterAtom);
+
+   return (
+     <Fragment>
+        {count}
+     </Fragment>
+   )
 }
 
-function CurrentCount() {
-  const count = useRecoilValue(counterAtom);
-  return <div>
-    {count}
-  </div>
-}
+function IsEven(){
 
-function Decrease() {
+   const even = useRecoilValue(evenSelector);
 
-  const setCount = useSetRecoilState(counterAtom);
-
-  function decrease() {
-    setCount(c => c - 1);
-  }
-
-  return <div>
-    <button onClick={decrease}>Decrease</button>
-  </div>
+   return (
+     <Fragment>
+       {even ? "Even" : "Odd"}
+     </Fragment>
+   )
 }
 
 
-function Increase() {
-  const setCount = useSetRecoilState(counterAtom);
+export default App;
 
-  function increase() {
-    setCount(c => c + 1);
-  }
-
-  return <div>
-    <button onClick={increase}>Increase</button>
-  </div>
-}
-
-export default App
+ 
--- React-Learning/week-11-recoil/src/store/atoms/counter.js
+++ React-Learning/week-11-recoil/src/store/atoms/counter.js
@@ -1,6 +1,15 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const counterAtom = atom({
   key: "counterAtom",  // Unique key
   default: 0,          // Default value
 });
+
+export const evenSelector = selector({
+   key: "isEvenSelector",
+   get: function({ get }) {
+          const currentCount = get(counterAtom);
+          const isEven = (currentCount % 2 == 0);
+          return isEven;
+   }
+})
