07cff5e2b14d3903c1e4715ef7801fea1e931f21
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-13 00:39:17 +0530

    Recoil -> Selector

--- React-Learning/week-11-recoil-offline/src/atoms.js
+++ React-Learning/week-11-recoil-offline/src/atoms.js
@@ -1,13 +1,16 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
+// Asynchronous data queries
 export const notifications = atom({
     key: "networkAtom",
-    default: {
-        network: 0,
-        jobs: 0,
-        notifications: 0,
-        messaging: 0
-    }
+    default: selector({
+        key: "networkAtomSelector",
+        git: async () => {
+            const res = await axios.get("https://sum-server.100xdevs.com/notification")
+            return res.data
+        }
+        
+    })
 });
 
 
