20ddd6ce906aed7386f6a51eb537c7876a4ce806
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-19 21:13:12 +0530

    Grid implement

--- week-13-tailwind04-class01/src/App.jsx
+++ week-13-tailwind04-class01/src/App.jsx
@@ -4,10 +4,10 @@ function App() {
 
   return (
     <>
-      <div className='flex justify-between'>
-        <div className='bg-blue-400'>child 1</div>
-        <div className='bg-yellow-400'>child 2</div>
-        <div className='bg-pink-400'>child 3</div>
+      <div className='grid grid-cols-12'>
+        <div className='bg-blue-400 col-span-4'>child 1</div>
+        <div className='bg-yellow-400 col-span-6'>child 2</div>
+        <div className='bg-pink-400 col-span-2'>child 3</div>
       </div>
     </>
   )
