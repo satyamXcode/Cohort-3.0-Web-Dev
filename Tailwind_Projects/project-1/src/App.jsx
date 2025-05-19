718217beaa2c7a0ff03a6c887d277bf38db88b1a
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-19 23:04:00 +0530

    Designing tailwind project

---
+++ Tailwind_Projects/project-1/src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+
+function App() {
+
+  return (
+    <Fragment>
+       <div className="bg-blue-400">I am going to design project in tailwind.</div>
+    </Fragment>
+  )
+}
+
+export default App
