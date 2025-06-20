268f4fad7e9cde1db869a0e1f5d268017c2bf61e
satyamXcode <jaiswalsatyam876@gmail.com>
2025-06-20 23:40:02 +0530

    Refactor code: BroadCasting Logic

--- WebSockets/Week-16-Part02/chat-app-backend/src/index.ts
+++ WebSockets/Week-16-Part02/chat-app-backend/src/index.ts
@@ -2,21 +2,18 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let userCount = 1;
 let allSockets: WebSocket[] = [];
 
 wss.on("connection", (socket) => {
-
   allSockets.push(socket);
 
-  userCount = userCount + 1;
-  console.log("user connnected #" + userCount);
+  console.log("user connnected #");
 
   socket.on("message", (message) => {
     console.log("message received " + message.toString());
-    for(let i = 0; i < allSockets.length; i++){
-        const s = allSockets[i];
-        s.send(message.toString() +": sent from the server");
+    for (let i = 0; i < allSockets.length; i++) {
+      const s = allSockets[i];
+      s.send(message.toString() + ": sent from the server");
     }
   });
 });
