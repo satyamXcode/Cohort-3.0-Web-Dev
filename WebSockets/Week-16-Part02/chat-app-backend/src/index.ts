90ade761e8cb2f86bc05f2ffbaba1ec1f055c22f
satyamXcode <jaiswalsatyam876@gmail.com>
2025-06-20 23:13:47 +0530

    Sent and response app logic completed

--- WebSockets/Week-16-Part02/chat-app-backend/src/index.ts
+++ WebSockets/Week-16-Part02/chat-app-backend/src/index.ts
@@ -5,6 +5,13 @@ const wss = new WebSocketServer({ port: 8080 });
 let userCount = 1;
 
 wss.on("connection", (socket) => {
-    userCount = userCount + 1;
-    console.log("user connnected #" + userCount);
-})
+  userCount = userCount + 1;
+  console.log("user connnected #" + userCount);
+
+  socket.on("message", (message) => {
+    console.log("message received " + message.toString());
+    setTimeout(() => {
+      socket.send(message.toString() +": sent from the server");
+    }, 1000);
+  });
+});
