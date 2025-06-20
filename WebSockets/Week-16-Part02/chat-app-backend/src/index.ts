e80ced4208a8f7bbc036f6b47f73a8fe4f580a7e
satyamXcode <jaiswalsatyam876@gmail.com>
2025-06-20 23:22:46 +0530

    Braodcasting logic done

--- WebSockets/Week-16-Part02/chat-app-backend/src/index.ts
+++ WebSockets/Week-16-Part02/chat-app-backend/src/index.ts
@@ -1,17 +1,22 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 let userCount = 1;
+let allSockets: WebSocket[] = [];
 
 wss.on("connection", (socket) => {
+
+  allSockets.push(socket);
+
   userCount = userCount + 1;
   console.log("user connnected #" + userCount);
 
   socket.on("message", (message) => {
     console.log("message received " + message.toString());
-    setTimeout(() => {
-      socket.send(message.toString() +": sent from the server");
-    }, 1000);
+    for(let i = 0; i < allSockets.length; i++){
+        const s = allSockets[i];
+        s.send(message.toString() +": sent from the server");
+    }
   });
 });
