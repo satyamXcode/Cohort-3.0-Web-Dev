cfcafcb4d9fdc4af56cad9085852e80cd99c6990
satyamXcode <jaiswalsatyam876@gmail.com>
2025-07-21 17:29:23 +0530

    First class completed

--- Prisma_And_ORMs/week-18-prisma/src/index.ts
+++ Prisma_And_ORMs/week-18-prisma/src/index.ts
@@ -1,1 +1,41 @@
-console.log("hi there");
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const client = new PrismaClient();
+
+app.get("/users", async (req, res) => {
+    const users = await client.user.findMany();
+    res.json({
+        users
+    })
+})
+
+app.get("/todos/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const user = await client.user.findFirst({
+        where: {
+            id: parseInt(id)
+        },
+        select: {
+            todos: true,
+            username: true,
+            password: true,
+        }
+    });
+    res.json({
+        user
+    })
+})
+
+async function createUser(){
+    await client.user.create({
+        data: {
+            username: "harkirat",
+            password: "12345",
+            age: 21,
+            city: "Delhi"
+        }
+    })
+}
